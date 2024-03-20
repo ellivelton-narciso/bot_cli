4ba03c8b9f0485cf4f68d27ba404e7f7b7c6b34a
Ellivelton Narciso <ellivelton.souza@gmail.com>
2024-03-20 03:41:16 +0000

    Adicionado funcao pra evitar o delay de 2s iniciais.

--- listar_ordens/listar_ordens.go
+++ listar_ordens/listar_ordens.go
@@ -4,10 +4,13 @@ import (
 	"binance_robot/config"
 	"binance_robot/database"
 	"binance_robot/models"
+	"binance_robot/util"
 	"encoding/json"
 	"errors"
+	"fmt"
 	"io/ioutil"
 	"net/http"
+	"os"
 	"strconv"
 	"time"
 )
@@ -64,6 +67,68 @@ func ListarUltimosValores(coin string) ([]models.HistoricoAll, error) {
 		return nil, errors.New("hist_trading_values retornou um array vazio")
 	}
 	return historicos, nil
+}
+
+func ListarUltimosValoresBuy(coin string, count int64) ([]models.PriceResponse, float64) {
+
+	config.ReadFile()
+
+	url := config.BaseURL + "fapi/v1/ticker/price?symbol=" + coin
+	req, _ := http.NewRequest("GET", url, nil)
+
+	response, err := http.DefaultClient.Do(req)
+	if err != nil {
+		util.WriteError("Erro ao acessar a API para converter: ", err, coin)
+		os.Exit(1)
+
+	}
+
+	defer response.Body.Close()
+	body, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	var priceR models.PriceResponse
+	err = json.Unmarshal(body, &priceR)
+	if err != nil {
+		fmt.Println("Erro ao decodificar JSON:", err)
+	}
+
+	if priceR.Price == "" {
+		fmt.Println("Preço é vazio, provavelmente devido a algum erro na requisição em momento de compra, , StatusCode: ", response.StatusCode, " Coin: ", coin)
+		var historicos []models.Historico
+		database.DB.Order("created_at DESC").Limit(int(count)).Find(&historicos)
+
+		var priceRespAll []models.PriceResponse
+		for _, historico := range historicos {
+			var data []models.PriceResponse
+			if err := json.Unmarshal([]byte(historico.Value), &data); err != nil {
+				fmt.Println("\n Erro ao decodificar JSON - ", err)
+				continue
+			}
+			priceRespAll = append(priceRespAll, data...)
+		}
+
+		var priceResp []models.PriceResponse
+		for _, item := range priceRespAll {
+			if item.Symbol == coin {
+				priceResp = append(priceResp, item)
+			}
+		}
+
+		return priceResp, 0
+	}
+
+	price, err := strconv.ParseFloat(priceR.Price, 64)
+
+	if err != nil {
+		fmt.Println("Erro ao converter preço para float64 em momento de compra: ", err)
+		return nil, 0
+	}
+
+	return nil, price
+
 }
 
 func ListarValorAnterior(coin string) (float64, error) {
