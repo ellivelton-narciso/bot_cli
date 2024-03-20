6c095294e35b993e74c338aad18eed978009e9c6
Ellivelton Narciso <ellivelton.souza@gmail.com>
2024-03-20 04:48:34 +0000

    Adicionado  listar ultimos valores da tabela historico por conta que estava sendo estrapolado o limite da api

--- listar_ordens/listar_ordens.go
+++ listar_ordens/listar_ordens.go
@@ -4,13 +4,11 @@ import (
 	"binance_robot/config"
 	"binance_robot/database"
 	"binance_robot/models"
-	"binance_robot/util"
 	"encoding/json"
 	"errors"
 	"fmt"
 	"io/ioutil"
 	"net/http"
-	"os"
 	"strconv"
 	"time"
 )
@@ -69,66 +67,30 @@ func ListarUltimosValores(coin string) ([]models.HistoricoAll, error) {
 	return historicos, nil
 }
 
-func ListarUltimosValoresBuy(coin string, count int64) ([]models.PriceResponse, float64) {
-
+func ListarUltimosValoresReais(coin string, count int64) []models.PriceResponse {
 	config.ReadFile()
 
-	url := config.BaseURL + "fapi/v1/ticker/price?symbol=" + coin
-	req, _ := http.NewRequest("GET", url, nil)
+	var historicos []models.Historico
+	database.DB.Order("created_at DESC").Limit(int(count)).Find(&historicos)
 
-	response, err := http.DefaultClient.Do(req)
-	if err != nil {
-		util.WriteError("Erro ao acessar a API para converter: ", err, coin)
-		os.Exit(1)
-
-	}
-
-	defer response.Body.Close()
-	body, err := ioutil.ReadAll(response.Body)
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	var priceR models.PriceResponse
-	err = json.Unmarshal(body, &priceR)
-	if err != nil {
-		fmt.Println("Erro ao decodificar JSON:", err)
-	}
-
-	if priceR.Price == "" {
-		fmt.Println("Preço é vazio, provavelmente devido a algum erro na requisição em momento de compra, , StatusCode: ", response.StatusCode, " Coin: ", coin)
-		var historicos []models.Historico
-		database.DB.Order("created_at DESC").Limit(int(count)).Find(&historicos)
-
-		var priceRespAll []models.PriceResponse
-		for _, historico := range historicos {
-			var data []models.PriceResponse
-			if err := json.Unmarshal([]byte(historico.Value), &data); err != nil {
-				fmt.Println("\n Erro ao decodificar JSON - ", err)
-				continue
-			}
-			priceRespAll = append(priceRespAll, data...)
+	var priceRespAll []models.PriceResponse
+	for _, historico := range historicos {
+		var data []models.PriceResponse
+		if err := json.Unmarshal([]byte(historico.Value), &data); err != nil {
+			fmt.Println("\n Erro ao decodificar JSON - ", err)
+			continue
 		}
+		priceRespAll = append(priceRespAll, data...)
+	}
 
-		var priceResp []models.PriceResponse
-		for _, item := range priceRespAll {
-			if item.Symbol == coin {
-				priceResp = append(priceResp, item)
-			}
+	var priceResp []models.PriceResponse
+	for _, item := range priceRespAll {
+		if item.Symbol == coin {
+			priceResp = append(priceResp, item)
 		}
-
-		return priceResp, 0
 	}
 
-	price, err := strconv.ParseFloat(priceR.Price, 64)
-
-	if err != nil {
-		fmt.Println("Erro ao converter preço para float64 em momento de compra: ", err)
-		return nil, 0
-	}
-
-	return nil, price
-
+	return priceResp
 }
 
 func ListarValorAnterior(coin string) (float64, error) {
