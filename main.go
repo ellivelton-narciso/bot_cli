5b652850a9b0b0b26a0a66ae8e935b4616570bb8
Ellivelton Narciso <67260567+ellivelton-narciso@users.noreply.github.com>
2024-03-27 18:10:03 +0000

    Corrigido Query, nao estava filtrando pelo tipo de alerta.

--- main.go
+++ main.go
@@ -21,7 +21,36 @@ func main() {
 
 	for {
 		bots = nil
-		if err := database.DB.Find(&bots).Error; err != nil {
+		if err := database.DB.Raw(`
+			SELECT * FROM v_selected_orders so
+				join (SELECT trading_name,
+					TIPO_ALERTA,
+					  trend,
+					  ROUND(total_win / total * 100, 2) perc_win
+				FROM (SELECT TIPO_ALERTA,
+							trend,
+							trading_name,
+							SUM(CASE WHEN status = 'W' THEN 1 ELSE 0 END) AS total_win,
+							COUNT(1) AS total
+					 FROM (SELECT ROUND(other_value) AS TIPO_ALERTA,
+								  trading_name,
+								  (CASE WHEN trend_value > 0 THEN 'LONG' ELSE 'SHORT' END) AS trend,
+								  status
+						   FROM findings_history a
+						   WHERE close_date > NOW() - INTERVAL 2 DAY
+							AND status IN ('W', 'L')
+							AND other_value IN (2, 3)) x
+					 GROUP BY TIPO_ALERTA, trading_name, trend
+					 HAVING COUNT(1) > 5) z
+						join bot_control bc ON bc.status = 'A'
+				WHERE bc.alert = z.TIPO_ALERTA
+				 AND bc.side = z.trend
+				 AND total_win / total >= 0.75
+				 AND total >= 6
+				ORDER BY perc_win DESC, total_win DESC) tp ON tp.trading_name = so.coin
+			WHERE tp.trend = so.tend
+			AND tp.TIPO_ALERTA = ROUND(so.other_value)
+		`).Scan(&bots).Error; err != nil {
 			log.Println("Erro ao buscar dados da tabela v_selected_orders:", err)
 			continue
 		}
