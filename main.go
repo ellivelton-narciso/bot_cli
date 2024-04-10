22844d4995f5c983a639b7d1bef7787516abcfc2
Ellivelton Narciso <ellivelton.souza@gmail.com>
2024-04-10 16:27:42 +0100

    Adicionada condição para validar se na tabela de controle o bot esta ativo caso nao esteja em ambiente de desenvolvimento.

--- main.go
+++ main.go
@@ -24,35 +24,49 @@ func main() {
 	}
 
 	for {
-		bots = nil
-		if err := database.DB.Find(&bots).Error; err != nil {
-			log.Println("Erro ao buscar dados da tabela v_selected_orders:", err)
-			time.Sleep(5 * time.Second)
+		var control models.Control
+		controle := database.DB.Raw("SELECT * FROM money_bot").First(&control)
+		if controle.Error != nil {
+			log.Println("Erro ao buscar control do bot")
 			continue
 		}
-		if len(bots) == 0 {
-			time.Sleep(1 * time.Second)
-			continue
+		if config.Development {
+			control.Ativo = "A"
+			control.Valor = config.Value
+			control.Alavancagem = config.Alavancagem
+		}
+		if control.Ativo == "A" {
+			bots = nil
+			if err := database.DB.Find(&bots).Error; err != nil {
+				log.Println("Erro ao buscar dados da tabela v_selected_orders:", err)
+				time.Sleep(5 * time.Second)
+				continue
+			}
+			if len(bots) == 0 {
+				time.Sleep(1 * time.Second)
+				continue
+			}
+
+			for _, bot := range bots {
+				go func(bot models.ResponseQuery) {
+					if bot.SL < 0 {
+						bot.SL = -(bot.SL)
+					}
+					if bot.SP < 0 {
+						bot.SP = -(bot.SP)
+					}
+					if bot.Tend == "SHORT" {
+						executarOrdem.OdemExecucao(bot.Coin, bot.Tend, control.Valor, control.Alavancagem, bot.SL, bot.SP, bot.OtherValue)
+						return
+
+					} else if bot.Tend == "LONG" {
+						executarOrdem.OdemExecucao(bot.Coin, bot.Tend, config.Value, config.Alavancagem, bot.SL, bot.SP, bot.OtherValue)
+						return
+					}
+				}(bot)
+			}
 		}
 
-		for _, bot := range bots {
-			go func(bot models.ResponseQuery) {
-				if bot.SL < 0 {
-					bot.SL = -(bot.SL)
-				}
-				if bot.SP < 0 {
-					bot.SP = -(bot.SP)
-				}
-				if bot.Tend == "SHORT" {
-					executarOrdem.OdemExecucao(bot.Coin, bot.Tend, config.Value, config.Alavancagem, bot.SL, bot.SP, bot.OtherValue)
-					return
-
-				} else if bot.Tend == "LONG" {
-					executarOrdem.OdemExecucao(bot.Coin, bot.Tend, config.Value, config.Alavancagem, bot.SL, bot.SP, bot.OtherValue)
-					return
-				}
-			}(bot)
-		}
 		time.Sleep(2 * time.Second)
 	}
 
