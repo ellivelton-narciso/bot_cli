b7d991ce86a535818c361e023f646af76c216173
Ellivelton Narciso <67260567+ellivelton-narciso@users.noreply.github.com>
2024-05-26 16:38:02 +0100

    chore: Adicionando o alerta 12 nas buscas

--- util/util.go
+++ util/util.go
@@ -280,7 +280,7 @@ func BuscarValoresTelegram(user string) []models.ResponseQuery {
 			  AND trend = 'SHORT'
 			  AND total > 1
 			  AND ROUND(total_win / total * 100, 2) >= 70
-			)) or (other_value = 51 AND trend_value > 0))
+			)) or (other_value = 51 AND trend_value > 0) or other_value = 12)
 		  and trading_name not in (select symbol from bots_real where user = ?)
 		  and status = 'R'
 		  AND hist_date > (NOW() - INTERVAL 1 MINUTE)
